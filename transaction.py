5d9f74f9f4c670aad99de4c2fab34694618e5843
wangqiyue <qwangcf@connect.ust.hk>
2024-05-30 19:36:37 +0800

    finish unit test in test_transaction.py

--- transaction.py
+++ transaction.py
@@ -1,12 +1,14 @@
 from bank_account import BankAccount
 
+
 class Item:
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
 
+
 class Transaction:
-    def __init__(self, buyer: BankAccount, seller: BankAccount,item: Item):
+    def __init__(self, buyer: BankAccount, seller: BankAccount, item: Item):
         self.buyer = buyer
         self.seller = seller
         self.item = item
